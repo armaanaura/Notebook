95ac9f98a13572df4799962054e4cdf4228b3436
armaanaura <armaanaurapvt@gmail.com>
2026-06-25 23:39:56 +0530

    Add CSES solution: CSES/DynamicProgramming/moneySums.java

---
+++ CSES/DynamicProgramming/moneySums.java
@@ -0,0 +1,46 @@
+/*
+Time limit: 1.00 s
+Memory limit: 512 MB
+
+You have n coins with certain values. Your task is to find all money sums you can create using these coins.
+Input
+The first input line has an integer n: the number of coins.
+The next line has n integers x_1,x_2,\dots,x_n: the values of the coins.
+Output
+First print an integer k: the number of distinct money sums. After this, print all possible sums in increasing order.
+Constraints
+
+1 \le n \le 100
+1 \le x_i \le 1000
+
+Example
+Input:
+4
+4 2 5 2
+
+Output:
+9
+2 4 5 6 7 8 9 11 13
+*/
+
+/*
+2^n - 1 are the total number of possible cases
+, there might be duplicates , so we need to return only distinct number of outputs
+*/
+import java.util.*;
+public class moneySums {
+    public static void memo(int[] coins, int index,int sum, HashSet<Integer>set, int[]dp){
+        if(index == coins.length){
+            set.add(sum);
+            return;
+        }
+        if(dp)
+        memo(coins, index+1, sum, set, dp);
+        memo(coins, index+1, sum+coins[index], set, dp);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+    }
+}
